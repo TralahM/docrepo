8e528209ffe5cf755eb173e32426f15fae1f0b08
TralahM <musyoki.brian@tralahtek.com>
2020-04-23 01:33:51 +0300

    refactoring templates and LICENSE to BSD3

---
+++ templates/funding.py
@@ -0,0 +1,5 @@
+funding_template = """
+github: {0}
+patreon: {0}
+# Custom: [{0}.github.io,TralahTek.github.io,]
+"""
---
+++ templates/readme.py
@@ -0,0 +1,48 @@
+readme_template = """
+[![Build Status](https://travis-ci.com/{1}/{0}.svg?branch=master)](https://travis-ci.com/{1}/{0})
+[![License: MIT](https://img.shields.io/badge/License-MIT-red.svg)](https://opensource.org/licenses/MIT)
+[![Organization](https://img.shields.io/badge/Org-TralahTek-blue.svg)](https://github.com/TralahTek)
+[![HitCount](http://hits.dwyl.io/{1}/{0}.svg)](http://dwyl.io/{1}/{0})
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](https://github.com/{1}/pull/)
+[![GitHub pull-requests](https://img.shields.io/github/issues-pr/Naereen/StrapDown.js.svg)](https://gitHub.com/{1}/{0}/pull/)
+[![GitHub version](https://badge.fury.io/gh/Naereen%2FStrapDown.js.svg)](https://github.com/{1}/{0}).
+
+# {0}.
+
+
+[![{1}](https://img.shields.io/badge/Engineer-{1}-blue.svg?style=for-the-badge)](https://github.com/{1})
+[![{1}](https://img.shields.io/badge/Maintainer-{1}-green.svg?style=for-the-badge)](https://github.com/{1})
+
+# Documentation
+
+# How to Install
+
+## Building from Source for Developers
+
+```Bash
+git clone https://github.com/{1}/{0}.git
+cd {0}
+```
+
+# Contributing
+[See the Contributing File](CONTRIBUTING.rst)
+
+
+[See the Pull Request File](PULL_REQUEST_TEMPLATE.md)
+
+
+# Support
+
+# LICENCE
+
+[Read the license here](LICENSE)
+
+
+# Acknowledgements
+
+[Find me on Twitter](https://twitter.com/TralahM)
+
+[Find me on Kaggle](https://kaggle.com/TralahM)
+
+
+"""
