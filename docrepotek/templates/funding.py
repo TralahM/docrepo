578e0b4598eb4ba541197ceddccdb99ee4154f70
TralahM <musyoki.brian@tralahtek.com>
2020-09-25 12:02:02 +0300

    paypal donate funding.yml

--- docrepotek/templates/funding.py
+++ docrepotek/templates/funding.py
@@ -1,5 +1,6 @@
 funding_template = """
 github: {0}
 patreon: {0}
+custom: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=WFKVHBRCYEE6S&source=url"
 # Custom: [{0}.github.io,TralahTek.github.io,]
 """
